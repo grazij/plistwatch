83319633cc1ce702c30e6bb5094483c3ea51def7
CatsDeservePets <145048791+CatsDeservePets@users.noreply.github.com>
2025-09-26 02:53:28 +0200

    Filter by domain names

--- main.go
+++ main.go
@@ -2,9 +2,13 @@ package main
 
 import (
 	"bytes"
+	"flag"
 	"fmt"
+	"maps"
 	"os"
 	"os/exec"
+	"path/filepath"
+	"strings"
 	"time"
 
 	"github.com/catilac/plistwatch/go-plist"
@@ -19,7 +23,51 @@ func getDefaults() (bytes.Buffer, error) {
 	return out, err
 }
 
+func filterDomains(m map[string]any, include, exclude []string) {
+	maps.DeleteFunc(m, func(k string, v any) bool {
+		// Allow every domain by default
+		if len(include) == 0 {
+			return false
+		}
+		for _, pattern := range include {
+			if matched, _ := filepath.Match(pattern, k); matched {
+				return false
+			}
+		}
+		return true
+	})
+	maps.DeleteFunc(m, func(k string, v any) bool {
+		for _, pattern := range exclude {
+			if matched, _ := filepath.Match(pattern, k); matched {
+				return true
+			}
+		}
+		return false
+	})
+}
+
 func main() {
+	var include []string
+	var exclude []string
+
+	flag.Func("filter", "a comma-separated list of `domains`. Prefix names with \"!\" to exclude them. Supports globbing.", func(s string) error {
+		for _, v := range strings.Split(s, ",") {
+			domain, found := strings.CutPrefix(strings.TrimSpace(v), "!")
+			// Users might write "! com.apple.dock" so we trim again
+			domain = strings.TrimSpace(domain)
+			if domain == "" {
+				continue
+			}
+			if found {
+				exclude = append(exclude, domain)
+			} else {
+				include = append(include, domain)
+			}
+		}
+		return nil
+	})
+	flag.Parse()
+
 	var prev map[string]interface{}
 	var curr map[string]interface{}
 
@@ -29,6 +77,8 @@ func main() {
 			fmt.Println(err)
 			os.Exit(-1)
 		}
+
+		filterDomains(curr, include, exclude)
 
 		if prev != nil {
 			if err = Diff(prev, curr); err != nil {
